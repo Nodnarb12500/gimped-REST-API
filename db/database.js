bdf4f2d5cdd20c220bf4c48f555e6312172cf6f1
nodnarb12500 <nodnarb12500@airmail.cc>
2023-06-15 22:47:06 -0400

    added pagination and started searching

--- db/database.js
+++ db/database.js
@@ -1,25 +1,8 @@
 const knex = require("./knex");
 const logging = require("../logging");
 
-/*
- * replace tableName with the name of the table you want to modify
- * and if you want change the names of the functions to reflect that change to
- */
 
-
-// check for the table and create it if !exists
-/*
-* Currently the table and all the modifications that might be done to the table
-* all expect to be provided an ID number. 
-* I dont see why username only wouldnt work so if preferable I might be able
-* to grab rows based on Username instead of the ID number
-*/
-
-
-
-function checkTable(tName){
-  let tableName = tName
-
+function checkTable(tableName){
   if (tableName == "userData") {
     knex.schema.hasTable(tableName).then(function(exists) {
       if (!exists) {
@@ -57,11 +40,10 @@ function checkTable(tName){
   }
 }
 
-
 /* Modifying the database */
-
 function createRow(table, data) {
-  // this should only ever run on the creation of a new user
+  // this should only ever run on the creation of a new user if specified
+  // This might also get used for creating posts!
   return knex(table).insert(data);
 }
 
@@ -78,10 +60,32 @@ function deleteRow(table, id) {
 /* Searching the database */
 
 function getRow(table, user) {
-  // this should also happen whenever a user syncs bookmarks but should happen last.
-  //return knex(table).where("id", id);
+  // this grabs a specific row and should not have pagination
+  //return knex(table).where("id", id); // im keeping this line because its what i normally use
   console.log("collecting " + user + "'s data from table " + table);
   return knex(table).where("username", user);
+}
+
+/* Pagination */
+function pagination(table, count, page) {
+  if (count => 100) {
+    // limit amount to 100
+    count = 100;
+  }
+  let n = count * page;
+
+  console.log("getting " + count + " rows!");
+  return knex(table).select('*').limit(count).offset(n);
+
+}
+
+/* Sorting */
+function searchDB(search, count, page) {
+  // this might get complicated!
+  // the search options might be separated by a | or some other delimiter
+  // searching should search the database with the first option THEN 
+  // nodeJS will do the rest of the searching putting the values that matched into the next array
+  // looping until we run out of search terms
 
 }
 
@@ -90,5 +94,7 @@ module.exports = {
   createRow,
   modifyRow,
   deleteRow,
-  getRow
+  getRow,
+  pagination,
+  searchDB
 }
