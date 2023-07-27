1eb1bb64da0c164bce90bf67ada565218fc49467
nodnarb12500 <nodnarb12500@airmail.cc>
2023-07-26 23:18:26 -0400

    started upload page and fixed some bugs

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const verification = require("../verification");
 const db = require("../db/database");
 const config = require("../config");
 
+const rootFolder = `${__dirname}/..`;
 
 /*
  * Before I forget what the hell I'm doing
@@ -23,23 +24,10 @@ const config = require("../config");
 
 // we are already in the /api path so /api should not be used here in the paths
 
-
-/**
- * TODO: 
- *  Make sure that if a database exists, 
- *  - and if it doesnt make one, if it should exist. 
- *    - otherwise fail and tell the user its invalid
- * 
- *  
- *  
- */
-
-
 /* CREATING/MODIFYING DATA */
-app
-.post("/create/:table", async (req, res) => {
+app.post("/create/:table", async (req, res) => {
   const validated = await verification.checkToken(req.body.username, req.body.verKey);
-  const table = "userData"; //this is a debug line
+  const table = "userData";
   apiRequest = verification.stripToken(req.body);
 
   // just noticed i didnt finish this yet lmao
@@ -64,17 +52,12 @@ app
     res.status(401).json({"ERROR": "Invalid Token"});
   }
 
-})
-
-.put("/create/:table", async (req,res) => {
-  const validated = await verification.checkToken(req.body.username, req.body.verKey);
-  const table = "userData"; //this is a debug line
 });
 
 app.post("/modify/:table", async (req, res) => {
   /* Get id and send back the JSON */
   const validated = await verification.checkToken(req.body.username, req.body.verKey);
-  const table = "userData"; //this is a debug line
+  const table = "userData";
   apiRequest = verification.stripToken(req.body);
 
   if (validated === true) {
@@ -92,22 +75,10 @@ app.post("/modify/:table", async (req, res) => {
 app.post("/rm/:table", async (req, res) => {
   // mark stuff for delete and hide instead?
   const validated = await verification.checkToken(req.body.username, req.body.verKey);
-  const table = "userData"; //this is a debug line
+  const table = "userData";
   apiRequest = verification.stripToken(req.body);
 
-
-  /**
-   * TODO:
-   *  Consider making delete a hidden thing, that requires another confirmination that then actually deletes the post
-   *      OR
-   *  require users to retype passwords, or "delete" to delete a file
-   * 
-   *  If deleteing a users account, remove that users database entry and tokens from the token file (if required)
-   *    - This needs to be tested, if a users entry is deleted does that token still allow use of the API that they shouldnt be able to access?
-   */
-
-
-  // require the user to retype their password for deleting their account, if this gets implemeted?
+  // require the user to retype their password for deleting their account
   // make sure to remove the users tokens
 
   if (validated === true) {
@@ -128,21 +99,6 @@ app.post("/get/:table", async (req, res) => {
   // this shoulnt actually need authentication
   const validated = await verification.checkToken(req.body.username, req.body.verKey);
 
-
-  /**
-   * TODO:
-   *  This is incorrect and needs to be looked at, this currently only grabs a row, is this what we want?
-   *  maybe have 1 grabbing single rows and another one that grabs several rows in a table unless this already does that.
-   * 
-   *  if (params.id !== null) then single grab
-   *  else row grab?
-   * 
-   *  ** This should NOT require a token to use otherwise all users of the site would require a password
-   *        - New setting idea? "require all users to have an account to access the API (this includes viewing posts, users without an account will not be able to view posts)"
-   * 
-   */
-
-
   if (validated === true) {
     const results = await db.getRow(req.params.id);
     res.status(200).json({results});
@@ -156,8 +112,8 @@ app.post("/get/:table", async (req, res) => {
 
 app
 .get("/search", (req, res) => {
+  res.status(200).sendFile("website/html/search.html", {root: rootFolder});
 
-  res.status(200).sendFile("/website/html/search.html", __dirname);
 })
 
 /*
@@ -169,6 +125,28 @@ app
 
 .post("/search", (req, res) => {
 
+});
+
+/**
+ * TODO:
+ *  User Uploads
+ *    - Uploads must accept
+ *      - JSON
+ *      - user Files
+ *    - the files must be compressed.
+ *    - the files must be placed in folders based on their upload date and filenames updated to reflect the perminite path for the files
+ * 
+ *
+ */
+
+app.get("/upload", (req, res) => {
+  // res.status(200).sendFile("/website/html/upload.html", {root: rootFolder});
+  res.status(200).sendFile("website/html/upload.html", {root: rootFolder});
+})
+
+.post("/upload", (req, res) => {
+
+  console.log(req);
 });
 
 /* Token Test */
