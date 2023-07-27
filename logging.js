47a2a2e46556ef751953536003f1ed7692927a62
nodnarb12500 <nodnarb12500@airmail.cc>
2023-07-26 23:16:10 -0400

    increased API limits for testing

--- logging.js
+++ logging.js
@@ -37,18 +37,8 @@ const getActualRequestDurationInMilliseconds = start => {
 }
 
 var limiter = RateLimit({
-
-    /**
-     * TODO:
-     *  add these as settings to the config.js file
-     * 
-     */
-
-    // 10 requests per minute per IP
     msWindow: 1*60*1000, // 1 minute?
-    max: 10 // how meny requests are allowed in the specified time limit above
-    
-
+    max: 1000 // how meny requests are allowed in the specified time limit above
 });
 
 function logging(message, loglevel) {
