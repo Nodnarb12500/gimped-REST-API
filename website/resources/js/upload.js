8ace44d20e4ffdb8c78b94da3baa7199aab0e4e5
nodnarb12500 <nodnarb12500@airmail.cc>
2023-08-05 01:28:08 -0400

    created client side upload.js

--- website/resources/js/upload.js
+++ website/resources/js/upload.js
@@ -8,3 +8,65 @@
  *      - both of these API calls should happen at the same time. (one can fail because of the API limits)
  *      - Downloads should be directed into their own folder and filepaths should be generated on the server
  */
+
+function start() {
+    // this file is for adding listeners to things
+
+    let uploadForm = document.getElementById("uploadForm")
+    let submitBtn = document.getElementById("submitBtn");
+    let fileUpload = document.getElementById("fileUpload")
+
+    uploadForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+    });
+
+    submitBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        upload();
+    })
+
+    // auto populate the file name box
+    fileUpload.addEventListener("change", (e) => {
+        e.preventDefault();
+        document.getElementById("fileName").value = fileUpload.value.slice(12);
+    });
+
+    document.getElementById("verKeyBox").value = cookie.get("verKey");
+
+}
+
+async function postRequest(formData, path) {
+    try {
+        const response = fetch(path, {
+            method: "POST",
+            body: formData,
+        });
+
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error("Error:", error)
+    }
+}
+
+function upload() {
+    // this is where the form information is gatherd before getting set to the server
+    // this form information can use formData objects since it needs to be a multi-part/form for file uploads!
+    // attempt to get authenication with cookies?
+
+    let formData = new FormData(document.getElementById("uploadForm"));
+    // add any other random form entries?
+
+
+    // send this via a post request and process the response!
+     
+
+
+    // wtf did we send to the server?
+    for (var pair of formData.entries()) {
+        console.log(pair[0]+ ', ' + pair[1]); 
+    }
+
+    postRequest(formData, "/upload");
+
+}
